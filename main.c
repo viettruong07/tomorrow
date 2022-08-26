52e23bfa9a72fba0c080d4323bfd57903410f90e
Trương Công Việt <truongviet0707@gmail.com>
2022-08-26 08:17:21 +0700

    bai tap 1

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  Hello
+//
+//  Created by Trương Công Việt on 24/08/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a;
+    printf("Hello, World! \n");
+    scanf("%d",&a);
+    printf("Hello, World! %d\n", a);
+    return 0;
+}
